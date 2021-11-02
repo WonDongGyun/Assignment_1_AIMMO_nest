ae86e38398a95e6ec2934e3dfe987a1e9968b69c
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-11-03 08:55:54 +0900

    Style: 띄어쓰기 수정
    한 칸 띄어쓰기 수정하였습니다.

--- src/domain/user/dto/createUser.dto.ts
+++ src/domain/user/dto/createUser.dto.ts
@@ -2,17 +2,17 @@ import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
 	@IsString()
-	@Length(4,12)
+	@Length(4, 12)
 	@IsNotEmpty()
 	userId!: string;
 
 	@IsString()
-	@Length(4,20)
+	@Length(4, 20)
 	@IsNotEmpty()
 	password!: string;
 
 	@IsString()
-	@Length(2,12)
+	@Length(2, 12)
 	@IsNotEmpty()
 	userName!: string;
 }
